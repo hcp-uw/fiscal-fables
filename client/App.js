83f973db0d3233d6342c628ebabf5bd161cb90bd
marnelram <marnelram@gmail.com>
2024-04-02 15:51:30 -0700

    setup Tamagui

--- client/App.js
+++ client/App.js
@@ -1,22 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import SomeComponent from './src/SomeComponent';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import SomeComponent from "./src/SomeComponent";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This is me testing how to use Expo Go!</Text>
-      <StatusBar style="auto" />
-      <SomeComponent />
-    </View>
+    <TamaguiProvider>
+      <View style={styles.container}>
+        <Text>This is me testing how to use Expo Go!</Text>
+        <StatusBar style="auto" />
+        <SomeComponent />
+      </View>
+    </TamaguiProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
