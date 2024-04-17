eb8d7810f30a772adbc5eabb836b2b284940071f
blloop <42186044+blloop@users.noreply.github.com>
2024-04-16 19:29:45 -0700

    add sample prifile routes

--- server/src/app.js
+++ server/src/app.js
@@ -17,9 +17,62 @@ app.get('/login', (req, res) => {
   console.log(req);
 });
 
-app.get("/users/:user/emails/:email", (req, res) => {
+// Example route
+app.get("/user/:user/email/:email", (req, res) => {
   // Access userId via: req.params.user
   // Access bookId via: req.params.email
+  res.send(req.params);
+});
+
+// loadApp()
+app.get("/username/:username", (req, res) => {
+  res.send(req.params);
+});
+
+// refreshProfile()
+app.get("/username/:username", (req, res) => {
+  // Fetch data from database
+  let data = [
+    {
+      name: "bill",
+      email: "google", 
+      coins: 100
+    }, 
+    {
+      name: "anupritaa", 
+      email: "yahoo", 
+      coins: 200
+    }, 
+    {
+      name: "johnny",
+      email: "hotmail",
+      coins: 100
+    }
+  ];
+
+  // Log param for debugging
+  console.log(req.params.username)
+  
+  // Look in data for username
+  let result = {};
+  if (data[0].name === req.params.username) {
+    // For each, check for matching username and return
+    res.send({
+      email: "",
+      coins: ""
+    });
+  } else {
+    res.send({});
+  }
+});
+
+// refreshHome()
+app.get("/home", (req, res) => {
+  res.send(req.params);
+});
+
+// getCourses()
+app.get("/courses", (req, res) => {
   res.send(req.params);
 });
 
