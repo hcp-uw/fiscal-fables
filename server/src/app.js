a7b7e0e884a86758765a3742ac8f2aa4df8d6ec4
blloop <42186044+blloop@users.noreply.github.com>
2024-04-20 18:18:03 -0700

    restructure app api

--- server/src/app.js
+++ server/src/app.js
@@ -5,127 +5,173 @@ const port = 3000;
 const app = express();
 app.use(cors());
 
-/** GET endpoint for sending back a Hello World message */
-app.get('/hello', (req, res) => {
-  res.type('text');
-  res.send('Hello, World!');
-  // console.log(req.params.Email);
-});
+/* Sample data values */
+
+let courseData = [
+  {
+    courseID: 1234,
+    courseImage: "image.com",
+    questions: [
+      {questionsText: "What is your name?",
+      coinValue: 100,
+      answers: [
+        {
+          isCorrect: false,
+          text: answer1
+        },
+
+        {
+          isCorrect: false,
+          text: answer2
+        },
+
+        {
+          isCorrect: false,
+          text: answer3
+        },
+
+        {
+          isCorrect: true,
+          text: answer4
+        }
+      ]}
+        
+    ]
+    }
+];
+
+let userData = [
+  {
+    name: "bill",
+    email: "google", 
+    coins: 100
+  },
+  {
+    name: "anupritaa", 
+    email: "yahoo", 
+    coins: 200
+  }, 
+  {
+    name: "johnny",
+    email: "hotmail",
+    coins: 100
+  }
+];
+
+/* Example routes */
 
 app.get('/login', (req, res) => {
   res.type('text');
   res.send('Hello, World!');
-  console.log(req);
+  // console.log(req);
 });
 
-// Example route
 app.get("/user/:user/email/:email", (req, res) => {
   // Access userId via: req.params.user
   // Access bookId via: req.params.email
   res.send(req.params);
 });
 
-// loadApp()
+/* Main Routes */
+
+/**
+ * loadApp()
+ * Parameters: 
+ * - username(string)
+ * Returns:
+ * - email(string)
+ * - coins(string)
+ * - courses(array of courses)
+ */
 app.get("/username/:username", (req, res) => {
   res.send(req.params);
+
+  // Get data from database
+
+  // For example, check the table to make the user exists
+
+  // ... Do some computation
+
+  // Return the requested data
+  res.send({
+    email: "",
+    coins: 100, 
+    courseData: []
+  })
 });
 
-// refreshProfile()
+/**
+ * refreshProfile()
+ * Parameters: TODO
+ * Returns: TODO
+ */
 app.get("/username/:username", (req, res) => {
   // Fetch data from database
-  let data = [
-    {
-      name: "bill",
-      email: "google", 
-      coins: 100
-    }, 
-    {
-      name: "anupritaa", 
-      email: "yahoo", 
-      coins: 200
-    }, 
-    {
-      name: "johnny",
-      email: "hotmail",
-      coins: 100
-    }
-  ];
-
+  
   // Log param for debugging
   console.log(req.params.username)
   
   // Look in data for username
-  let result = {};
-  if (data[0].name === req.params.username) {
-    // For each, check for matching username and return
-    res.send({
-      email: "",
-      coins: ""
-    });
-  } else {
-    res.send({});
+  // let result = {};
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].name === req.params.username) {
+      res.send({
+        email: data[i].email,
+        coins: data[i].coins
+      })
+    }
   }
+
+  // Else, return null
+  res.send({});
 });
 
-// refreshHome()
+/**
+ * refreshHome()
+ * Parameters: TODO
+ * Returns: TODO
+ */
 app.get("/home", (req, res) => {
   res.send(req.params);
 });
 
-// getCourses()
+/**
+ * getCourses()
+ * Parameters: TODO
+ * Returns: TODO
+ */
 app.get("/courses", (req, res) => {
   res.send(req.params);
 });
 
-//getUserLogin()
+/**
+ * getUserLogin()
+ * Parameters: TODO
+ * Returns: TODO
+ */
 app.get("/username/:username/email/:email/password/:password", (req, res) => {
 
   res.send(req.params);
 });
 
-//getCourseByID()
+/**
+ * getCourseById()
+ * Parameters: TODO
+ * Returns: TODO
+ */
 app.get("/courseID/:courseID", (req, res) => {
 
   //Fetch courses data
 
-  let courses = [
-    {
-      courseID: 1234,
-      courseImage: "image.com",
-      questions: [
-        {questionsText: "What is your name?",
-        coinValue: 100,
-        answers: [
-          {
-            isCorrect: false,
-            text: answer1
-          },
-
-          {
-            isCorrect: false,
-            text: answer2
-          },
-
-          {
-            isCorrect: false,
-            text: answer3
-          },
-
-          {
-            isCorrect: true,
-            text: answer4
-          }
-        ]}
-          
-      ]
-      }
-      
-
-  ]
+  
   res.send(req.params);
 });
 
-//getAnswer()
+/**
+ * getAnswer()
+ * Parameters: TODO
+ * Returns: TODO
+ */
 app.get("/courseID/:courseID/qNumber/:qNumber", (req, res) => {
   res.send(req.params);
 });
